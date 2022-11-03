f1ccef88f7afaa31cfe819b1609c0b50605236f9
Dmytro Zinkevych <dzinkevych@gmail.com>
2022-11-03 22:28:26 +0100

    override equals() and hashCode() in FSMTransition and FSMStateActions

--- src/main/java/io/github/dmytrozinkevych/finitafsm/FSMTransition.java
+++ src/main/java/io/github/dmytrozinkevych/finitafsm/FSMTransition.java
@@ -1,8 +1,25 @@
 package io.github.dmytrozinkevych.finitafsm;
 
+import java.util.Objects;
+
 public record FSMTransition(
         FSMState oldState,
         FSMEvent event,
         TriConsumer<FSMState, FSMEvent, FSMState> action,
         FSMState newState
-) { }
+) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FSMTransition that = (FSMTransition) o;
+        return Objects.equals(oldState, that.oldState)
+                && Objects.equals(event, that.event)
+                && Objects.equals(newState, that.newState);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(oldState, event, newState);
+    }
+}
